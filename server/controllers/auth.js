84f9a2a4912f4efd6338a4bb04dc749d9b9cba10
Kenneth <kkmphele@gmail.com>
2018-01-28 21:37:21 +0200

    implemented authentication endpoints in server

---
+++ server/controllers/auth.js
@@ -0,0 +1,78 @@
+var User = require('../models/user');
+
+var jwt = require('jwt-simple');
+var moment=require('moment');
+
+
+module.exports =
+    {
+        register: function (req, res) {
+             console.log(req.body.username);
+
+ 
+
+        User.findOne({ username: req.body.username}, function (err, existingUser) {
+
+            if (existingUser) {
+                return res.status(409).send({ message: "email already registerd" })
+            }
+            var user = new User(req.body);
+
+
+            user.save(function (err, result) {
+                if (err) {
+                    res.status(500).send({ message: err.message });
+                }
+                res.status(200).send({ token: createToken(result) });
+
+            })
+
+        })
+
+        },
+
+        login: function (req, res) {
+            console.log(req)
+
+            User.findOne({
+                username: req.body.username
+            }, function (err, user) {
+
+                if (err) throw err;
+
+                if (!user) {
+                    res.json({ success: false, message: 'Authentication failed. User not found.' });
+                } else if (user) {
+
+                    if (user.password != req.body.password) {
+                        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+                    } else {
+
+                        var token = createToken(user);
+
+                        res.json({
+                            success: true,
+                            message: 'Enjoy your token!',
+                            token: token
+                        })
+
+
+                    }
+                }
+            })
+
+        },
+
+    }
+
+
+function createToken(user) {
+    var payload = {
+        sub: user._id,
+        ia: moment().unix(),
+        exp: moment().add(14, 'days').unix()
+    };
+
+    return jwt.encode(payload, 'secret');
+
+}
---
+++ server/models/user.js
@@ -0,0 +1,7 @@
+var mongoose=require('mongoose');
+
+module.exports=mongoose.model('User',{
+    username:String,
+    name:String,
+    password:String
+});
